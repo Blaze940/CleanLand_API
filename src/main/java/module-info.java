7dc9ad67ae89a0532da37f262787910f6ab372ad
Emmanuel <manuboisset@gmail.com>
2022-12-03 19:03:31 +0100

    change javafx to spring boot app

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.cleanlandapi.cleanland_api {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.cleanlandapi.cleanland_api to javafx.fxml;
-    exports com.cleanlandapi.cleanland_api;
-}
